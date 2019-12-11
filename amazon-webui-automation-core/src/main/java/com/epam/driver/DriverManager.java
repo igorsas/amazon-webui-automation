da2a7f43c75cc262cfd47a0f3116b00f2d667a1d
Igor_Sas <Igor_Sas@epam.com>
2019-12-11 16:03:15 +0200

    try to clear driver after every method

--- amazon-webui-automation-core/src/main/java/com/epam/driver/DriverManager.java
+++ amazon-webui-automation-core/src/main/java/com/epam/driver/DriverManager.java
@@ -1,36 +1,39 @@
 package com.epam.driver;
 
 import com.epam.utils.PropertyLoader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static com.epam.constant.GeneralConstants.DRIVER_PROPERTIES_NAME;
 
 public class DriverManager {
-    private static ThreadLocal<WebDriver> DRIVER_POOL = new ThreadLocal<>();
-    private static final String NAME = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME,"name"));
-    private static final String PATH = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME,"path"));
+    private static final String NAME = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "name"));
+    private static final String PATH = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "path"));
     private static final boolean HEADLESS_MODE = Boolean.parseBoolean(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "headless"));
+    private static ThreadLocal<WebDriver> DRIVER_POOL = new ThreadLocal<>();
 
     static {
         System.setProperty(NAME, PATH);
     }
 
-    private DriverManager() { }
+    private DriverManager() {
+    }
 
-    public static WebDriver getDriver()
-    {
-        if(Objects.isNull(DRIVER_POOL.get())) {
+    public static WebDriver getDriver() {
+        if (Objects.isNull(DRIVER_POOL.get())) {
             initializeDriver();
         }
         return DRIVER_POOL.get();
     }
 
-    private static void initializeDriver(){
+    private static void initializeDriver() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(HEADLESS_MODE);
         options.addArguments("disable-infobars");
@@ -47,5 +50,14 @@ public class DriverManager {
     public static void quit() {
         DRIVER_POOL.get().quit();
         DRIVER_POOL.set(null);
+    }
+
+    public static void clearCache() {
+        DRIVER_POOL.get().manage().deleteAllCookies();
+        //doesn't work
+        DRIVER_POOL.get().close();
+        DRIVER_POOL.get().findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL +"t");
+        ArrayList<String> tabs = new ArrayList<String> (DRIVER_POOL.get().getWindowHandles());
+        DRIVER_POOL.get().switchTo().window(tabs.get(1));
     }
 }
--- amazon-webui-automation-test/src/test/java/com/epam/AbstractTest.java
+++ amazon-webui-automation-test/src/test/java/com/epam/AbstractTest.java
@@ -2,18 +2,26 @@ package com.epam;
 
 import com.epam.driver.DriverManager;
 import com.epam.utils.PropertyLoader;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import static com.epam.constant.GeneralConstants.DRIVER_PROPERTIES_NAME;
+import static com.epam.driver.DriverManager.clearCache;
 
 public abstract class AbstractTest {
-    @BeforeMethod
+    @BeforeClass
     public void setStartedPage() {
         DriverManager.getDriver().get(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "initial_url"));
     }
 
-    @AfterMethod
+    @BeforeMethod
+    public void clearDriver() {
+        clearCache();
+
+    }
+
+    @AfterClass
     public void quitDriver() {
         DriverManager.quit();
     }
