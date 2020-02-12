76fe3057bf8c3f94534b68c685e1c89c7f48bb50
Igor_Sas <Igor_Sas@epam.com>
2020-02-12 19:10:27 +0200

    add selenium grid with docker-compose(need to investigate issue with webDriver)

--- amazon-webui-automation-core/src/main/java/com/epam/driver/DriverManager.java
+++ amazon-webui-automation-core/src/main/java/com/epam/driver/DriverManager.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +20,7 @@ public class DriverManager {
     private static final String NAME = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "name"));
     private static final String PATH = Objects.requireNonNull(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "path"));
     private static final boolean HEADLESS_MODE = Boolean.parseBoolean(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "headless"));
+    private static final boolean RUN_ON_HUB = Boolean.parseBoolean(PropertyLoader.getValue(DRIVER_PROPERTIES_NAME, "hub"));
     private static ThreadLocal<WebDriver> DRIVER_POOL = new ThreadLocal<>();
 
     static {
@@ -33,13 +38,23 @@ public class DriverManager {
     }
 
     private static void initializeDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.setHeadless(HEADLESS_MODE);
-        options.addArguments("disable-infobars");
+
+        WebDriver driver = null;
+        if (RUN_ON_HUB) {
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            try {
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(HEADLESS_MODE);
+            options.addArguments("disable-infobars");
 //        uncomment when need addblock
 //        options.addExtensions(new File("src/main/resources/adBlockExtension.crx"));
-        ChromeDriver driver = new ChromeDriver(options);
-
+            driver = new ChromeDriver(options);
+        }
         long implicitlyWait = Long.parseLong(Objects.requireNonNull(PropertyLoader.getValue(WAIT_TIMERS_PROPERTIES_NAME, "implicitlyWait")));
         long pageLoadTimeout = Long.parseLong(Objects.requireNonNull(PropertyLoader.getValue(WAIT_TIMERS_PROPERTIES_NAME, "pageLoadTimeout")));
         long scriptTimeout = Long.parseLong(Objects.requireNonNull(PropertyLoader.getValue(WAIT_TIMERS_PROPERTIES_NAME, "scriptTimeout")));
